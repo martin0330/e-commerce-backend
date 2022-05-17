52edc0ae06781a2f8e0023586e5b8262afd13473
martin0330 <martin.cruz0330@yahoo.com>
2022-05-17 13:05:10 -0500

    fixed bugs

--- models/index.js
+++ models/index.js
@@ -18,7 +18,7 @@ Product.belongsToMany(Tag, {
   foreignKey: 'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
-TimeRanges.belongsToMany(Products, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id'
 });
